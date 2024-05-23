29cdcf6b861b71af9a3681640fe6eac02b099d72
hutao <983210535@qq.com>
2024-05-23 15:16:35 +0800

    upd

--- db/sql.js
+++ db/sql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 let db = mysql.createConnection({
-  host: 'localhost',
+  host: '139.196.174.228',
   port: '3306',
   user: 'root',
-  password: '12345678',
+  password: '123456',
   database: 'gpt',
   authPlugins: {
     mysql_clear_password: () => () => Buffer.from(password + '\0')
